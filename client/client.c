b7ab720e81a2bd129bb137071c73a5829aeb72c1
Jack Desmond <f004hbw@plank.thayer.dartmouth.edu>
2023-02-26 16:49:44 -0500

    finished client section of implementation spec and updated the design spec

---
+++ client/client.c
@@ -0,0 +1,16 @@
+/* 
+ * client.c - The client allows a human to join a nuggets
+ * game as an interactive player or as a spectator. If a player name
+ * is given the human joins as a player. Otherwise, they join as a
+ * spectator
+ * 
+ * Usage: ./client hostname port [playerName]
+ * 
+ * Jack Desmond - February 25th, 2023
+ * CS50 Winter 2023
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
